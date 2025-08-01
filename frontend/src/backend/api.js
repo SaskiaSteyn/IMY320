abcb5a09b73cf96bccc10ba2988819c8ebf0d77a
JohanJvR <u22590732@tuks.co.za>
2025-08-01 18:02:52 +0200

    Create api.js

---
+++ frontend/src/backend/api.js
@@ -0,0 +1,31 @@
+const API_URL = 'http://localhost:5000';
+
+export async function register(userData) {
+    try {
+        const response = await fetch(`${API_URL}/register`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(userData),
+        });
+        return await response.json();
+    } catch (error) {
+        return error;
+    }
+}
+
+export async function login(email, password) {
+    try {
+        const response = await fetch(`${API_URL}/login`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({email, password}),
+        });
+        return await response.json();
+    } catch (error) {
+        return error;
+    }
+}
